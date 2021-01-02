a0944e850ef710e19d9023e36e884892a285cdc1
madhav-web <madhav.goel2019@vitstudent.ac.in>
2021-01-02 13:47:38 +0530

    Added a Procfile

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get("/",(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-  console.log("Server UP on port 3000");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
